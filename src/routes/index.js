b16c96646a7b8a7b5c097e83b3dbb8964b601c6c
Namdh157 <127576912+Namdh157@users.noreply.github.com>
2025-03-07 10:24:13 +0700

    delete: product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,7 @@
 import express from "express";
-import routerProduct from "./product.js";
 import routerAuth from "./auth.js";
 const router = express.Router();
 
-router.use('/product', routerProduct);
 router.use('/auth', routerAuth );
 
 export default router;
--- src/validation/category.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi";
-
-export const categoryValidator = Joi.object({
-
-    name: Joi.string().required().min(3).max(255),
-    slug: Joi.string().required().min(3).max(255),
-    
-})
