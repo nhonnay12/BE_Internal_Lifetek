f63c20d37cd72104fdca9bed9275e7f84e6a55c7
Namdh157 <127576912+Namdh157@users.noreply.github.com>
2025-03-10 16:00:56 +0700

    authController, uploadImage,worker,connect redis

---
+++ src/config/redisClient.js
@@ -0,0 +1,23 @@
+import { createClient } from "redis";
+
+const redisClient = createClient({
+    host: process.env.REDIS_HOST || "localhost",
+    port: process.env.REDIS_PORT || 6379,
+});
+
+redisClient.on("error", (error) => {
+    console.error("Redis error: ", error);
+});
+
+const connectRedis = () => {
+   try {
+        redisClient.on("connect", () => {
+             console.log("Redis connected");
+        });
+
+   } catch (error) {
+        console.error("Connect redis error: ", error);
+   }
+}
+
+export { redisClient, connectRedis };
---
+++ src/services/emailService.js
@@ -0,0 +1,21 @@
+import transporter from "../config/nodeMailer.js";
+
+const sendMail = async ({ to, subject, text, html }) => {
+    try {
+        const info = await transporter.sendMail({
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            text,
+            html
+        });
+
+        return info;
+
+    } catch (error) {
+        console.error(error);
+        throw new Error("Gui mail that bai");
+    }
+}
+
+export default sendMail;
