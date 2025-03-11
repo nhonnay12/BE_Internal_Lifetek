61f9a1c3fe5b6278bc6d178679ed9ad62655d6be
Namdh157 <127576912+Namdh157@users.noreply.github.com>
2025-03-11 10:17:51 +0700

    create,update: Issues

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import routerAuth from "./auth.js";
 import routerProject from "./projectRoute.js";
+import routeIssues from "./issuesRoute.js";
 const router = express.Router();
 
 router.use('/auth', routerAuth);
 router.use('/project', routerProject);
+router.use('/issue', routeIssues);
 
 export default router;
