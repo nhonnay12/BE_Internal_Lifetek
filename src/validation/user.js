1c769661714fdcf4bfbe4488065b6e044e5a9e99
Namdh157 <127576912+Namdh157@users.noreply.github.com>
2025-03-11 09:04:50 +0700

    modify:auth_controller, validate_user

--- src/validation/user.js
+++ src/validation/user.js
@@ -1,7 +1,7 @@
 import Joi from "joi"
 
 export const signUpValidator = Joi.object({
-     userName: Joi.string().required().min(6).max(255).messages({
+     userName: Joi.string().required().min(6).max(30).messages({
         "string.empty": "userName không được để trống",
         "any.required": "userName là bắt buộc",
         "string.min": "userName phải có ít nhất {#limit} ký tự",
@@ -12,13 +12,13 @@ export const signUpValidator = Joi.object({
         "any.required": "email là bắt buộc",
         "string.email": "email không đúng định dạng",
      }),
-     password: Joi.string().required().min(6).max(255).messages({
+     password: Joi.string().required().min(6).max(30).messages({
         "string.empty": "Password không được để trống",
         "any.required": "Password là bắt buộc",
         "string.min": "Password phải có ít nhất {#limit} ký tự",
         "string.max": "Password phải có ít hơn {#limit + 1} ký tự",
      }),
-     confirmPassword: Joi.string().required().min(6).max(255).valid(Joi.ref("password")).messages({
+     confirmPassword: Joi.string().required().min(6).max(30).valid(Joi.ref("password")).messages({
         "string.empty": "confirmPassword không được để trống",
         "any.required": "confirmPassword là bắt buộc",
         "string.min": "confirmPassword phải có ít nhất {#limit} ký tự",
@@ -35,7 +35,7 @@ export const signInValidator = Joi.object({
       "any.required": "email là bắt buộc",
       "string.email": "email không đúng định dạng",
    }),
-   password: Joi.string().required().min(6).max(255).messages({
+   password: Joi.string().required().min(6).max(30).messages({
       "string.empty": "Password không được để trống",
       "any.required": "Password là bắt buộc",
       "string.min": "Password phải có ít nhất {#limit} ký tự",
