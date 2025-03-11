6110cd3ff8bc9682707b568c9cc6b6b6c679f81f
kquan2312 <95995990+kquan2312@users.noreply.github.com>
2025-03-11 09:54:37 +0700

    them api add comment3

--- src/validation/comment.js
+++ src/validation/comment.js
@@ -2,15 +2,23 @@ import Joi from "joi";
 
 export const validateComment = (req, res, next) => {
   const schema = Joi.object({
-    projectId: Joi.string().hex().length(24).required(),
-    taskId: Joi.string().hex().length(24).required(),
     userId: Joi.string().hex().length(24).required(),
-    issueId: Joi.string().hex().length(24).required(),
     content: Joi.string().min(3).max(500).required(),
   });
 
-  const { error } = schema.validate(req.body);
-  if (error) return res.status(400).json({ message: error.details[0].message });
+  const paramsSchema = Joi.object({
+    projectId: Joi.string().hex().length(24).required(),
+    taskId: Joi.string().hex().length(24).required(),
+    issueId: Joi.string().hex().length(24).required(),
+  });
+
+  const { error: bodyError } = schema.validate(req.body);
+  if (bodyError)
+    return res.status(400).json({ message: bodyError.details[0].message });
+
+  const { error: paramsError } = paramsSchema.validate(req.params);
+  if (paramsError)
+    return res.status(400).json({ message: paramsError.details[0].message });
 
   next();
 };
