29f95b5f5458431f427270dfdc19684dd0539714
tdaov <tdaovan154@gmail.com>
2025-04-01 23:37:40 +0700

    day len nhanh main

--- src/workers/index.js
+++ src/workers/index.js
@@ -1,13 +1,13 @@
-const { Worker } = require("bullmq");
-const dotenv = require("dotenv");
-const { emailQueue } = require("../queues/index.js");
-const emailTask = require("../queues/Tasks/sendMailQueue.js");
+// const { Worker } = require("bullmq");
+// const dotenv = require("dotenv");
+// const { emailQueue } = require("../queues/index.js");
+// const emailTask = require("../queues/Tasks/sendMailQueue.js");
 
-dotenv.config();
+// dotenv.config();
 
-const connection = {
-    host: process.env.REDIS_HOST || "localhost",
-    port: process.env.REDIS_PORT || 6379, 
-};
+// const connection = {
+//     host: process.env.REDIS_HOST || "localhost",
+//     port: process.env.REDIS_PORT || 6379, 
+// };
 
-new Worker(emailQueue.name, emailTask, { connection });
+// new Worker(emailQueue.name, emailTask, { connection });
